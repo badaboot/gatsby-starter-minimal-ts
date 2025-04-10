998d79d71d473454b73c9545365e7b82ebe8ec31
Anny He <annyannyhehe@gmail.com>
2025-04-09 22:18:22 -0700

    fix images

--- src/pages/blog/{markdownRemark.frontmatter__slug}.jsx
+++ src/pages/blog/{markdownRemark.frontmatter__slug}.jsx
@@ -3,13 +3,11 @@ import { graphql } from "gatsby";
 import Layout from "../../components/layout";
 import Header from "../../components/header";
 
-export default function BlogPostTemplate({
-  data, // this prop will be injected by the GraphQL query below.
-}) {
-  const { markdownRemark } = data; // data.markdownRemark holds your post data
+export default function BlogPostTemplate({ data }) {
+  const { markdownRemark } = data;
   const { frontmatter, html } = markdownRemark;
   return (
-    <Layout>
+    <Layout className="post">
       <Header textAlign="center" siteTitle={frontmatter.title} />
       <h3 style={{ textAlign: "center" }}>{frontmatter.date}</h3>
       <div dangerouslySetInnerHTML={{ __html: html }} />
